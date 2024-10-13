28f7f388bcf71092ff56652fc7c5115ff4a2e432
Armand LE VALLOIS <armand.le_vallois@telecom-sudparis.eu>
2024-10-13 22:46:29 +0300

    add database connection

--- db.php
+++ db.php
@@ -1,13 +1,14 @@
 <?php
-$host = 'localhost';
-$dbname = 'CSinventory';
-$username = 'root';  // Replace with your database username
-$password = '';      // Replace with your database password
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$port = getenv('DB_PORT') ?: '3306';  // Default to 3306 for MySQL
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo 'Connection failed: ' . $e->getMessage();
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
